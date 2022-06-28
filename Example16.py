17f1062c5d842732da1e16c59b0b77e2e6d93748
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-28 11:45:55 -0500

    R22 (chlorodifluoromethane) added, but pDynamo is giving "untyped atoms" errors for it and it seems to be in the yaml files that some charges on bonds are not defined or whatever. Also adding LAMMPS to try this out with Nix

--- Example16.py
+++ Example16.py
@@ -8,7 +8,11 @@ mmModel = MMModelOPLS ( "bookSmallExamples" )
 nbModel = NBModelFull ( )
 
 # . Generate the molecule.
-molecule = MOLFile_ToSystem ( os.path.join ( molPath, "bala_c7eq.mol" ) )
+#moleculeFileName="bala_c7eq.mol"
+moleculeFileName="r22.mol" # How I grabbed it: https://pubchem.ncbi.nlm.nih.gov/compound/Chlorodifluoromethane#section=3D-Conformer and then click download -> SDF. Then convert this file using http://www.cheminfo.org/Chemistry/Cheminformatics/FormatConverter/index.html and select SDF as the input format. "Untyped atoms" [maybe?] means the definition isn't in `/nix/store/x165imivrma61v7y9c9jc7d2c78dfpil-pDynamo2-1.9.0/parameters/forceFields/opls/bookSmallExamples/atomTypes.yaml` (including the "Charge" attribute -- all the "Parameter Values" are labelled right above in the "Parameter Fields" section -- so convenient.
+# More info on the MOL format: https://chem.libretexts.org/Courses/University_of_Arkansas_Little_Rock/ChemInformatics_(2017)%3A_Chem_4399_5399/2.2%3A_Chemical_Representations_on_Computer%3A_Part_II/2.2.2%3A_Anatomy_of_a_MOL_file
+#molecule = MOLFile_ToSystem ( os.path.join ( molPath, moleculeFileName ) )
+molecule = MOLFile_ToSystem ( os.path.join ( moleculeFileName ) )
 molecule.DefineMMModel ( mmModel )
 molecule.DefineNBModel ( nbModel )
 molecule.Summary ( )
@@ -44,7 +48,7 @@ VelocityVerletDynamics_SystemGeometry ( molecule                               ,
                                         temperatureStart          =      300.0 )
 
 # . Data-collection.
-trajectory = SystemGeometryTrajectory ( os.path.join ( scratchPath, "bala_c7eq.trj" ), molecule, mode = "w" )
+trajectory = SystemGeometryTrajectory ( os.path.join ( scratchPath, os.path.splitext(moleculeFileName)[0] + ".trj" ), molecule, mode = "w" )
 VelocityVerletDynamics_SystemGeometry ( molecule             ,
                                         logFrequency =   500 ,
                                         steps        = 100000 ,
---
+++ LAMMPS_Testing/shell.nix
@@ -0,0 +1,25 @@
+# { pkgs ? import (builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
+#   # Descriptive name to make the store path easier to identify
+#   name = "nixos-unstable-2022-06-02";
+#   # Commit hash for nixos-unstable as of the date above
+#   url = "https://github.com/NixOS/nixpkgs/archive/d2a0531a0d7c434bd8bb790f41625e44d12300a4.tar.gz";
+#   # Hash obtained using `nix-prefetch-url --unpack <url>`
+#   sha256 = "13nwivn77w705ii86x1s4zpjld6r2l197pw66cr1nhyyhi5x9f7d";
+# }) { }}:
+# with pkgs;
+
+{ pkgs ? import (builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-2020-09-03";
+  # Commit hash for nixos-unstable as of the date above
+  url = "https://github.com/NixOS/nixpkgs/archive/702d1834218e483ab630a3414a47f3a537f94182.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1vs08avqidij5mznb475k5qb74dkjvnsd745aix27qcw55rm0pwb";
+}) { }}:
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    (callPackage ./lammps.nix {}) #lammps
+  ];
+}
