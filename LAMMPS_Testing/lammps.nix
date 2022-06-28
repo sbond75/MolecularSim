9b11017e1791fcafaad5405b9506307df657a113
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-28 12:50:18 -0500

    Tweaking which LAMMPS packages are enabled

--- LAMMPS_Testing/lammps.nix
+++ LAMMPS_Testing/lammps.nix
@@ -5,7 +5,7 @@
 , withMPI ? false
 , mpi
 , gcc
-, pkg-config, openmp, callPackage
+, pkg-config, openmp, llvmPackages
 }:
 let packages = [
       # All packages (from https://github.com/lammps/lammps/blob/7d5fc356fefa1dd31d64b1cc856134b165febb8a/src/Makefile ) :
@@ -50,7 +50,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ fftw libpng blas lapack gzip gcc
-                  pkg-config openmp (callPackage ./kim-api.nix {}) ]
+                  pkg-config llvmPackages.openmp (callPackage ./kim-api.nix {}) ]
     ++ (lib.optionals withMPI [ mpi ]);
 
   configurePhase = ''
