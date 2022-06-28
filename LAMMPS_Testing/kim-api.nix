6975f3bff2430339c44c12fe8d95952ed05b7ae8
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-28 12:52:38 -0500

    Tweaking which LAMMPS packages are enabled

--- LAMMPS_Testing/kim-api.nix
+++ LAMMPS_Testing/kim-api.nix
@@ -1,7 +1,7 @@
 # Based on instructions in https://github.com/openkim/kim-api/blob/master/INSTALL
 
 { lib, stdenv, fetchFromGitHub
-, cmake, pkg-config, doxygen, graphviz, bash-completion
+, cmake, pkg-config, doxygen, graphviz, bash-completion, gfortran
 }:
 
 stdenv.mkDerivation rec {
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     sha256 = "1al1sb9zabb7pdiylky1linm2d61a1pkwmdaylcp9rr08ssgr3ak";
   };
 
-  buildInputs = [ cmake pkg-config doxygen graphviz bash-completion ];
+  buildInputs = [ cmake pkg-config doxygen graphviz bash-completion gfortran ];
 
   # Note: here we let Nix determine the `configurePhase`, `buildPhase`, and `installPhase`, but here are some possible examples (but they are probably not as useful as the prebuilt ones in https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/tools/build-managers/cmake/setup-hook.sh which is called by https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/tools/build-managers/cmake/default.nix ) :
   
