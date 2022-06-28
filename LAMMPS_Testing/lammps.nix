53f73638bdabbd4bd7e8cbccf26dede5fd43e14d
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-28 12:12:00 -0500

    Tweaking which LAMMPS packages are enabled

--- LAMMPS_Testing/lammps.nix
+++ LAMMPS_Testing/lammps.nix
@@ -7,12 +7,23 @@
 }:
 let packages = [
       # All packages (from https://github.com/lammps/lammps/blob/7d5fc356fefa1dd31d64b1cc856134b165febb8a/src/Makefile ) :
+      # "adios" "asphere" "awpmd" "bocs" "body" "bpm" "brownian" "cg-dna" "cg-sdk" "class2" "colloid"
+      # "colvars" "compress" "coreshell" "diffraction" "dipole" "dpd-basic" "dpd-meso" "dpd-react"
+      # "dpd-smooth" "drude" "eff" "extra-compute" "extra-dump" "extra-fix" "extra-molecule"
+      # "extra-pair" "fep" "gpu" "granular" "h5md" "intel" "interlayer" "kim" "kokkos" "kspace"
+      # "latboltz" "latte" "machdyn" "manifold" "manybody" "mc" "mdi" "meam" "mesont" "mgpt" "misc"
+      # "ml-hdnnp" "ml-pace" "ml-quip" "ml-rann" "ml-snap" "mofff" "molecule" "molfile" "mpiio" "mscg"
+      # "netcdf" "openmp" "opt" "orient" "peri" "plugin" "plumed" "poems" "ptm" "python" "qeq" "qmmm"
+      # "qtb" "reaction" "reaxff" "replica" "rigid" "scafacos" "shock" "smtbq" "sph" "spin" "srd"
+      # "tally" "uef" "voronoi" "vtk" "yaff" "atc" "dielectric" "electrode" "ml-iap" "phonon"
+
+      # Packages except some with additional library dependencies:
       "adios" "asphere" "awpmd" "bocs" "body" "bpm" "brownian" "cg-dna" "cg-sdk" "class2" "colloid"
       "colvars" "compress" "coreshell" "diffraction" "dipole" "dpd-basic" "dpd-meso" "dpd-react"
       "dpd-smooth" "drude" "eff" "extra-compute" "extra-dump" "extra-fix" "extra-molecule"
       "extra-pair" "fep" "gpu" "granular" "h5md" "intel" "interlayer" "kim" "kokkos" "kspace"
       "latboltz" "latte" "machdyn" "manifold" "manybody" "mc" "mdi" "meam" "mesont" "mgpt" "misc"
-      "ml-hdnnp" "ml-pace" "ml-quip" "ml-rann" "ml-snap" "mofff" "molecule" "molfile" "mpiio" "mscg"
+      "ml-hdnnp" "ml-pace" "ml-rann" "ml-snap" "mofff" "molecule" "molfile" "mpiio" "mscg"
       "netcdf" "openmp" "opt" "orient" "peri" "plugin" "plumed" "poems" "ptm" "python" "qeq" "qmmm"
       "qtb" "reaction" "reaxff" "replica" "rigid" "scafacos" "shock" "smtbq" "sph" "spin" "srd"
       "tally" "uef" "voronoi" "vtk" "yaff" "atc" "dielectric" "electrode" "ml-iap" "phonon"
