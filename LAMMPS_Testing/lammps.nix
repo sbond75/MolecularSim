9a1a1b0cd545cb38091013f0d1157bff282b6739
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-28 13:35:54 -0500

    Tweaking which LAMMPS packages are enabled

--- LAMMPS_Testing/lammps.nix
+++ LAMMPS_Testing/lammps.nix
@@ -56,7 +56,7 @@ stdenv.mkDerivation rec {
   cmakeFlags =
     (builtins.map (pkg: "-DPKG_${lib.toUpper pkg}=yes") packages) # Based on https://docs.lammps.org/Build_package.html and https://docs.lammps.org/Build_cmake.html
       ++ "cmake" # Sets the location of CMakeLists.txt to be in the folder https://github.com/lammps/lammps/tree/develop/cmake
-  
+  ;
   
   # configurePhase = ''
   #   cd src
