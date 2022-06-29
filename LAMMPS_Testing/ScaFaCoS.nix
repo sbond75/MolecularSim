f9434f4edaa101d0c85fb478bef87771644be658
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-29 00:10:44 -0500

    Tweaking which LAMMPS packages are enabled

--- LAMMPS_Testing/ScaFaCoS.nix
+++ LAMMPS_Testing/ScaFaCoS.nix
@@ -16,7 +16,20 @@ stdenv.mkDerivation rec {
     "--with-internal-fftw"
     "--with-internal-pfft"
     "--with-internal-pnfft"
+    #"--with-pic" # Optional, use if `if(BUILD_SHARED_LIBS)` is true in the CMakeLists.txt in `https://github.com/lammps/lammps/tree/0bc6373386cdaccd7d675d994210b3aa0edfe639/cmake/CMakeLists.txt`
   ];
+
+  buildPhase = ''make''; # (`all` target doesn't exist)
+
+  # wip trying to fix: {"
+  # WARNING: No cycle counter found.  FFTW will use ESTIMATE mode
+  #          for all plans.  See the manual for more information.
+  # "} when configure phase is running; it seems to want cycle.h on https://github.com/scafacos/scafacos/blob/6fb9549bf6219f890a6b492d4780d1afe0ed2bc1/lib/common/fftw-3.3/configure.ac
+  # patchPhase = ''
+  #   substituteInPlace lib/common/fftw-3.3/configure.ac --replace \
+  #     "CPPFLAGS=\"$CPPFLAGS -I$srcdir/kernel\" \
+  #     "CPPFLAGS=\"$CPPFLAGS -I${} -I$srcdir/kernel\""
+  # '';
 
   src = fetchFromGitHub {
     owner = name;
