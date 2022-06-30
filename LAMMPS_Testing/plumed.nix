da8e7ec9d7fa2570138c584faa90ad4ad1dd3ec4
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-06-30 14:46:52 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/plumed.nix
+++ LAMMPS_Testing/plumed.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "plumed2";
   version = "2.7.4";
 
-  buildInputs = [ cmake python mpi blas lapack (callPackage ./vmx.nix {}) zlib ];
+  buildInputs = [ cmake python mpi blas lapack (callPackage ./vmd.nix {}) zlib ];
 
   src = fetchFromGitHub {
     owner = "plumed";
