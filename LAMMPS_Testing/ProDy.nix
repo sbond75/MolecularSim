45bd05f1f2e1443c36e2c0953dde8ac8e4ba46f2
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-01 16:57:13 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/ProDy.nix
+++ LAMMPS_Testing/ProDy.nix
@@ -10,6 +10,6 @@ buildPythonPackage rec {
   };
 
   propagatedBuildInputs = [
-    pythonPackages.numpy (callPackage ./biopython.nix {}) pythonPackages.pyparsing pythonPackages.scipy
+    pythonPackages.numpy (callPackage ./biopython.nix {buildPythonPackage=buildPythonPackage;}) pythonPackages.pyparsing pythonPackages.scipy
   ];
 }
