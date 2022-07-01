5634640bae69c7b39561924dda75ecd06f9988ce
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-01 14:49:32 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/lammps.nix
+++ LAMMPS_Testing/lammps.nix
@@ -82,7 +82,7 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ fftw libpng blas lapack gzip gcc
-                  pkg-config llvmPackages.openmp (callPackage ./kim-api.nix {}) bc cmake python git (callPackage ./voro.nix {}) unixtools.xxd netcdf gsl gfortran (callPackage ./ScaFaCoS.nix {}) eigen vtk (callPackage ./LATTE.nix {}) curl zstd (callPackage ./MSCG.nix {}) hdf5 (lib.optional withMKL mkl) opencl-headers tbb ffmpeg clang-tools (callPackage ./plumed.nix {fetchPypi=fetchPypi;})
+                  pkg-config llvmPackages.openmp (callPackage ./kim-api.nix {}) bc cmake python git (callPackage ./voro.nix {}) unixtools.xxd netcdf gsl gfortran (callPackage ./ScaFaCoS.nix {}) eigen vtk (callPackage ./LATTE.nix {}) curl zstd (callPackage ./MSCG.nix {}) hdf5 (lib.optional withMKL mkl) opencl-headers tbb ffmpeg clang-tools (callPackage ./plumed.nix {fetchPypi=fetchPypi; buildPythonPackage=buildPythonPackage;})
                 ]
     ++ (lib.optionals withMPI [ mpi ]);
 
