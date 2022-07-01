c064f7b8052c19e0751f8406234ad8da43780307
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-01 16:52:31 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/shell.nix
+++ LAMMPS_Testing/shell.nix
@@ -20,6 +20,6 @@ with pkgs;
 
 mkShell {
   buildInputs = [
-    (callPackage ./lammps.nix {withMPI = true; fetchPypi=python.fetchPypi; buildPythonPackage=python.buildPythonPackage;}) #lammps
+    (callPackage ./lammps.nix {withMPI = true; fetchPypi=python.fetchPypi; buildPythonPackage=buildPythonPackage;}) #lammps
   ];
 }
