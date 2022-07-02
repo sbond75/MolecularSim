a473bfd3b57cfaf7e3f7b0f57f891264f7cd43c4
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-02 12:54:16 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.); untested overlay code, will move it to just overriding icc and icpc compilers for intel compilers instead of overriding nix's cc/gcc

--- LAMMPS_Testing/shell.nix
+++ LAMMPS_Testing/shell.nix
@@ -15,8 +15,31 @@
   url = "https://github.com/NixOS/nixpkgs/archive/702d1834218e483ab630a3414a47f3a537f94182.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "1vs08avqidij5mznb475k5qb74dkjvnsd745aix27qcw55rm0pwb";
-}) { }}:
-with pkgs;
+})}:
+#with pkgs;
+
+# Apply overlay
+let
+  myOverlay = (self: super: { #(final: prev: {
+    nur = import (builtins.fetchTarball {
+      # Get the revision by choosing a version from https://github.com/nix-community/NUR/commits/master
+      url = "https://github.com/nix-community/NUR/archive/9edfb0c8f3fb110ec46216b648be2cbbd3592346.tar.gz";
+      # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
+      sha256 = "04387lzgl8y555b3lkz9aiw9xsldfg4zmzp930m62qw8zbrvrshd";
+    }); # https://discourse.nixos.org/t/problems-setting-up-nur/10690 , readme on https://github.com/nix-community/NUR
+    stdenv = nur.repos.dguibert.intel-compilers-overlay.stdenv;
+    # stdenv =
+    #   (super.overrideCC super.stdenv super.gcc).override
+    #     { cc = super.stdenv.cc; }; # Based on https://gist.github.com/Pitometsu/18b8f98bb9d63f38324cd3da7e9c0b9b
+  }); # this is two lambdas (curried + nested)
+  nixpkgs = import pkgs {};
+  finalPkgs = import pkgs {
+    #system = if (nixpkgs.hostPlatform.isDarwin) then "x86_64-darwin" else builtins.currentSystem; # For M1 Mac to (hopefully) work
+    # Identity: overlays = [];
+    overlays = [ myOverlay ];
+  };
+in
+with finalPkgs;
 
 mkShell {
   buildInputs = [
