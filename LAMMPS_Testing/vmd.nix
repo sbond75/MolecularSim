85ba90ce6651f5a87f8f8942db6841638c3754a9
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-02 15:13:34 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/vmd.nix
+++ LAMMPS_Testing/vmd.nix
@@ -34,6 +34,7 @@ stdenv.mkDerivation rec {
      ];
 
   configureFlags = []
+    # These match up with `vmd-${version}/Makefile` under linux.amd64.opengl :
                    ++ (lib.splitString " " ("LINUXAMD64 OPENGL OPENGLPBUFFER FLTK TK ACTC " +
                                             (if withCuda then "CUDA " else "") +
                                             "IMD LIBSBALL XINERAMA XINPUT " +
@@ -245,10 +246,10 @@ cp $prog ../binaries' #'cp $prog.intel64 ../binaries/$(basename "$prog")' # TODO
   # '';
 
   preInstall = ''
-    make -j $NIX_BUILD_CORES linux.amd64.opengl
+    #make -j $NIX_BUILD_CORES linux.amd64.opengl # Optional, it reconfigures for using ICC instead of GCC though and we already have this set up in a `configureFlags` declaration within Nix code above.
 
     cd src
-    #make veryclean
+    make veryclean
     make -j $NIX_BUILD_CORES
   '';
 }
