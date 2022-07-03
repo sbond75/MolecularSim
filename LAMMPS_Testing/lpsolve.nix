cb3249bca281e14c7ca4af054687b23e217ae436
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-03 13:24:25 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/lpsolve.nix
+++ LAMMPS_Testing/lpsolve.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ autoreconfHook ];
 
   src = fetchurl {
-    url = "mirror://sourceforge/${name}-${version}.tar.gz";
+    url = "mirror://sourceforge/sourceforge.net/project/${name}/${name}/${version}/lp_solve_${version}_source.tar.gz"; # https://versaweb.dl.sourceforge.net/project/lpsolve/lpsolve/5.5.2.11/lp_solve_5.5.2.11_source.tar.gz
     sha256 = "295f29c32ecf33c4704f48144bdff565acb3a013bc68516a6de3c2b71671bf20";
   };
 }
