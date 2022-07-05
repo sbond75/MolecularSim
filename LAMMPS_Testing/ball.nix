683099cde62baa43fcd9d74455595c94958a5cf1
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-04 20:29:36 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/ball.nix
+++ LAMMPS_Testing/ball.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, callPackage, cmake, flex, bison, fftw, openbabel2, libsvm, useCUDA ? true, cudatoolkit, tbb, mpi, libsForQt5, eigen, glew, python3 }:
+{ lib, stdenv, fetchFromGitHub, callPackage, cmake, flex, bison, fftw, openbabel2, libsvm, useCUDA ? true, cudatoolkit, tbb, mpi, qt4, eigen, glew, python3, gsl }:
 
 stdenv.mkDerivation rec {
   name = "ball";
@@ -6,7 +6,10 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ cmake flex bison fftw openbabel2 (callPackage ./lpsolve.nix {}) libsvm ]
                 ++ (lib.optional useCUDA cudatoolkit)
-                ++ [ tbb mpi libsForQt5.qt5.qtwebengine libsForQt5.qt5.qtbase libsForQt5.qt5.qtwebsockets libsForQt5.qt5.qt3d libsForQt5.qt5.qtnetworkauth libsForQt5.qt5.qtwebchannel eigen glew python3 libsForQt5.qt5.wrapQtAppsHook ];
+                ++ [ tbb mpi
+                     #libsForQt5.qt5.qtwebengine libsForQt5.qt5.qtbase libsForQt5.qt5.qtwebsockets libsForQt5.qt5.qt3d libsForQt5.qt5.qtnetworkauth libsForQt5.qt5.qtwebchannel libsForQt5.qt5.wrapQtAppsHook
+                     qt4
+                     eigen glew python3 gsl ];
 
   cmakeFlags = (if useCUDA then [ "-DUSE_CUDA=YES" ] else []) ++ [ "-DBALL_LICENSE=GPL" "-DUSE_MPI=YES" ];
   
