92faa8ea3e195091fffe1bdf38d7c4a7cb2670ac
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-05 09:54:39 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/ball.nix
+++ LAMMPS_Testing/ball.nix
@@ -13,18 +13,77 @@ stdenv.mkDerivation rec {
                      eigen glew python3 gsl doxygen tetex blas boost ];
 
   patchPhase = ''
+    # Remove tabs from CMakeLists.txt since substituteInPlace doesn't appear to be able to handle them:
+    expand -i CMakeLists.txt
+
     repl1=$(cat <<- "EOF"
   '' +
 "# Generate CMake package configuration for BALL build tree
 CONFIGURE_FILE(
-\\t\"\${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in\"
-\\t\"\${PROJECT_BINARY_DIR}/cmake/BALLConfig.cmake\"
-\\t@ONLY
+        \"\${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in\"
+        \"\${PROJECT_BINARY_DIR}/cmake/BALLConfig.cmake\"
+        @ONLY
 )
+
+# Generate BALL package config version file
+write_basic_package_version_file(
+        \"\${PROJECT_BINARY_DIR}/cmake/BALLConfigVersion.cmake\"
+        VERSION \${PROJECT_VERSION}
+        COMPATIBILITY AnyNewerVersion
+)
+
+# Generate exports
+EXPORT(TARGETS BALL
+        FILE \${PROJECT_BINARY_DIR}/cmake/BALLExport.cmake)
+
+IF(BALL_HAS_VIEW)
+        EXPORT(TARGETS VIEW
+                APPEND
+                FILE \${PROJECT_BINARY_DIR}/cmake/BALLExport.cmake)
+ENDIF()
+
+
+# Store BALL build directory in the CMake user package registry
+EXPORT(PACKAGE BALL)
+
+
+# Generate CMake package configuration for BALL installation
+IF(NOT APPLE)
+        # Installation path for BALL CMake package configuration files
+        SET(BALL_CMAKE_EXPORT_PATH \${CMAKE_INSTALL_LIBDIR}/cmake/BALL CACHE PATH \"Path to the cmake package configuration files\")
+
+        LIST(REMOVE_ITEM BALL_INCLUDE_DIRS \"\${PROJECT_BINARY_DIR}/include\")
+        LIST(REMOVE_ITEM BALL_INCLUDE_DIRS \"\${PROJECT_SOURCE_DIR}/include\")
+        SET(BALL_INCLUDE_DIRS \${BALL_PATH}/include/ \${BALL_INCLUDE_DIRS})
+
+        CONFIGURE_FILE(
+                \"\${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in\"
+                \"\${PROJECT_BINARY_DIR}/exports/BALLConfig.cmake\"
+                @ONLY
+        )
+
+        INSTALL(FILES
+                \"\${PROJECT_BINARY_DIR}/exports/BALLConfig.cmake\"
+                \"\${PROJECT_BINARY_DIR}/cmake/BALLConfigVersion.cmake\"
+                DESTINATION \"\${BALL_CMAKE_EXPORT_PATH}/\"
+                COMPONENT   \${COMPONENT_LIBBALL_DEV}
+        )
+
+        INSTALL(EXPORT BALLExportGroup
+                DESTINATION \${BALL_CMAKE_EXPORT_PATH}
+                FILE BALLExport.cmake
+                COMPONENT \"\${COMPONENT_LIBBALL_DEV}\")
+ENDIF()
+
+
+######################################################
+# Generate CTags for BALL project
+######################################################
+INCLUDE(BALLCTags)
 EOF
 )
 " + ''
-    repl1=$(echo -e "$repl1")
+    echo "$repl1"
 
     substituteInPlace CMakeLists.txt --replace "''${Python3_LIBRARIES}" "" \
       --replace "$repl1" ""
