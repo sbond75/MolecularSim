8e623827ce3162fcf65852f9fe88b894dbe94fc0
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-05 11:39:15 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/ball.nix
+++ LAMMPS_Testing/ball.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, callPackage, cmake, flex, bison, fftw, openbabel2, libsvm, useCUDA ? true, cudatoolkit, tbb, mpi, qt4, eigen, glew, python, gsl, doxygen, tetex, blas, boost, fetchPypi, buildPythonPackage }:
+{ lib, stdenv, fetchFromGitHub, callPackage, cmake, flex, bison, fftw, openbabel2, libsvm, useCUDA ? true, cudatoolkit, tbb, mpi, qt4, eigen, glew, python, gsl, doxygen, tetex, blas, boost, fetchPypi, buildPythonPackage, ntirpc }:
 
 stdenv.mkDerivation rec {
   name = "ball";
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
                 ++ [ tbb mpi
                      #libsForQt5.qt5.qtwebengine libsForQt5.qt5.qtbase libsForQt5.qt5.qtwebsockets libsForQt5.qt5.qt3d libsForQt5.qt5.qtnetworkauth libsForQt5.qt5.qtwebchannel libsForQt5.qt5.wrapQtAppsHook
                      qt4
-                     eigen glew python gsl doxygen tetex blas boost (callPackage ./sip.nix {fetchPypi=fetchPypi; buildPythonPackage=buildPythonPackage;}) ];
+                     eigen glew python gsl doxygen tetex blas boost (callPackage ./sip.nix {fetchPypi=fetchPypi; buildPythonPackage=buildPythonPackage;}) ntirpc ];
 
   patchPhase = ''
     repl1=$(cat <<- "EOF"
@@ -30,6 +30,8 @@ EOF
 
     # Prevent `error: friend declaration of` [...] `specifies default arguments and isn't a definition` (where `[...]` is something like `'std::istream& getline(std::istream&, BALL::String&, char)'`)
     substituteInPlace include/BALL/DATATYPE/string.h --replace "String& string,  char delimiter = '\n');" "String& string,  char delimiter);"
+    # Fix `error: cannot bind non-const lvalue reference of type 'boost::shared_ptr<BALL::PersistentObject>&' to an rvalue of type 'boost::shared_ptr<BALL::PersistentObject>'`
+    substituteInPlace /build/source/source/FORMAT/PDBFileDetails.C --replace 'boost::shared_ptr<PersistentObject> temp_ptr(new CrystalInfo());' 'const boost::shared_ptr<PersistentObject> temp_ptr(new CrystalInfo());'
   '';
 
   cmakeFlags = (if useCUDA then [ "-DUSE_CUDA=YES" ] else []) ++ [ "-DBALL_LICENSE=GPL" "-DUSE_MPI=YES" "-Wno-dev" ];
