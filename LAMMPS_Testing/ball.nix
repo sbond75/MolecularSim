b4c2e387c7632e7f0fdf1b7ac32d294cdcfb0c1a
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-04 20:57:04 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/ball.nix
+++ LAMMPS_Testing/ball.nix
@@ -13,71 +13,74 @@ stdenv.mkDerivation rec {
                      eigen glew python3 gsl doxygen tetex blas boost ];
 
   patchPhase = ''
+    repl1=$(cat <<- "EOF"
+# Generate CMake package configuration for BALL build tree
+CONFIGURE_FILE(
+	"''${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in"
+	"''${PROJECT_BINARY_DIR}/cmake/BALLConfig.cmake"
+	@ONLY
+)
+
+# Generate BALL package config version file
+write_basic_package_version_file(
+	"''${PROJECT_BINARY_DIR}/cmake/BALLConfigVersion.cmake"
+	VERSION ''${PROJECT_VERSION}
+	COMPATIBILITY AnyNewerVersion
+)
+
+# Generate exports
+EXPORT(TARGETS BALL
+	FILE ''${PROJECT_BINARY_DIR}/cmake/BALLExport.cmake)
+
+IF(BALL_HAS_VIEW)
+	EXPORT(TARGETS VIEW
+		APPEND
+		FILE ''${PROJECT_BINARY_DIR}/cmake/BALLExport.cmake)
+ENDIF()
+
+
+# Store BALL build directory in the CMake user package registry
+EXPORT(PACKAGE BALL)
+
+
+# Generate CMake package configuration for BALL installation
+IF(NOT APPLE)
+	# Installation path for BALL CMake package configuration files
+	SET(BALL_CMAKE_EXPORT_PATH ''${CMAKE_INSTALL_LIBDIR}/cmake/BALL CACHE PATH "Path to the cmake package configuration files")
+
+	LIST(REMOVE_ITEM BALL_INCLUDE_DIRS "''${PROJECT_BINARY_DIR}/include")
+	LIST(REMOVE_ITEM BALL_INCLUDE_DIRS "''${PROJECT_SOURCE_DIR}/include")
+	SET(BALL_INCLUDE_DIRS ''${BALL_PATH}/include/ ''${BALL_INCLUDE_DIRS})
+
+	CONFIGURE_FILE(
+		"''${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in"
+		"''${PROJECT_BINARY_DIR}/exports/BALLConfig.cmake"
+		@ONLY
+	)
+
+	INSTALL(FILES
+		"''${PROJECT_BINARY_DIR}/exports/BALLConfig.cmake"
+		"''${PROJECT_BINARY_DIR}/cmake/BALLConfigVersion.cmake"
+		DESTINATION "''${BALL_CMAKE_EXPORT_PATH}/"
+		COMPONENT   ''${COMPONENT_LIBBALL_DEV}
+	)
+
+	INSTALL(EXPORT BALLExportGroup
+		DESTINATION ''${BALL_CMAKE_EXPORT_PATH}
+		FILE BALLExport.cmake
+		COMPONENT "''${COMPONENT_LIBBALL_DEV}")
+ENDIF()
+
+
+######################################################
+# Generate CTags for BALL project
+######################################################
+INCLUDE(BALLCTags)
+EOF
+)
+
     substituteInPlace CMakeLists.txt --replace "''${Python3_LIBRARIES}" "" \
-      --replace <<-"EOF"
-    # Generate CMake package configuration for BALL build tree
-    CONFIGURE_FILE(
-      "''${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in"
-      "''${PROJECT_BINARY_DIR}/cmake/BALLConfig.cmake"
-      @ONLY
-    )
-
-    # Generate BALL package config version file
-    write_basic_package_version_file(
-      "''${PROJECT_BINARY_DIR}/cmake/BALLConfigVersion.cmake"
-      VERSION ''${PROJECT_VERSION}
-      COMPATIBILITY AnyNewerVersion
-    )
-
-    # Generate exports
-    EXPORT(TARGETS BALL
-      FILE ''${PROJECT_BINARY_DIR}/cmake/BALLExport.cmake)
-
-    IF(BALL_HAS_VIEW)
-      EXPORT(TARGETS VIEW
-        APPEND
-        FILE ''${PROJECT_BINARY_DIR}/cmake/BALLExport.cmake)
-    ENDIF()
-
-
-    # Store BALL build directory in the CMake user package registry
-    EXPORT(PACKAGE BALL)
-
-
-    # Generate CMake package configuration for BALL installation
-    IF(NOT APPLE)
-      # Installation path for BALL CMake package configuration files
-      SET(BALL_CMAKE_EXPORT_PATH ''${CMAKE_INSTALL_LIBDIR}/cmake/BALL CACHE PATH "Path to the cmake package configuration files")
-
-      LIST(REMOVE_ITEM BALL_INCLUDE_DIRS "''${PROJECT_BINARY_DIR}/include")
-      LIST(REMOVE_ITEM BALL_INCLUDE_DIRS "''${PROJECT_SOURCE_DIR}/include")
-      SET(BALL_INCLUDE_DIRS ''${BALL_PATH}/include/ ''${BALL_INCLUDE_DIRS})
-
-      CONFIGURE_FILE(
-        "''${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in"
-        "''${PROJECT_BINARY_DIR}/exports/BALLConfig.cmake"
-        @ONLY
-      )
-
-      INSTALL(FILES
-        "''${PROJECT_BINARY_DIR}/exports/BALLConfig.cmake"
-        "''${PROJECT_BINARY_DIR}/cmake/BALLConfigVersion.cmake"
-        DESTINATION "''${BALL_CMAKE_EXPORT_PATH}/"
-        COMPONENT   ''${COMPONENT_LIBBALL_DEV}
-      )
-
-      INSTALL(EXPORT BALLExportGroup
-        DESTINATION ''${BALL_CMAKE_EXPORT_PATH}
-        FILE BALLExport.cmake
-        COMPONENT "''${COMPONENT_LIBBALL_DEV}")
-    ENDIF()
-
-
-    ######################################################
-    # Generate CTags for BALL project
-    ######################################################
-    INCLUDE(BALLCTags)
-  EOF ""
+      --replace "$repl1" ""
   '';
 
   cmakeFlags = (if useCUDA then [ "-DUSE_CUDA=YES" ] else []) ++ [ "-DBALL_LICENSE=GPL" "-DUSE_MPI=YES" ];
