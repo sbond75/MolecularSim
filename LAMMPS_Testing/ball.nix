2898fb2f3b40a726bad7106c2be0a6f6076b212e
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-05 10:20:12 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/ball.nix
+++ LAMMPS_Testing/ball.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, callPackage, cmake, flex, bison, fftw, openbabel2, libsvm, useCUDA ? true, cudatoolkit, tbb, mpi, qt4, eigen, glew, python3, gsl, doxygen, tetex, blas, boost }:
+{ lib, stdenv, fetchFromGitHub, callPackage, cmake, flex, bison, fftw, openbabel2, libsvm, useCUDA ? true, cudatoolkit, tbb, mpi, qt4, eigen, glew, python, gsl, doxygen, tetex, blas, boost }:
 
 stdenv.mkDerivation rec {
   name = "ball";
@@ -10,30 +10,22 @@ stdenv.mkDerivation rec {
                 ++ [ tbb mpi
                      #libsForQt5.qt5.qtwebengine libsForQt5.qt5.qtbase libsForQt5.qt5.qtwebsockets libsForQt5.qt5.qt3d libsForQt5.qt5.qtnetworkauth libsForQt5.qt5.qtwebchannel libsForQt5.qt5.wrapQtAppsHook
                      qt4
-                     eigen glew python3 gsl doxygen tetex blas boost ];
+                     eigen glew python gsl doxygen tetex blas boost ];
 
   patchPhase = ''
-    # Remove tabs from CMakeLists.txt since substituteInPlace doesn't appear to be able to handle them:
-    expand -i CMakeLists.txt > CMakeLists2.txt
-    mv CMakeLists2.txt CMakeLists.txt
-
-    cat CMakeLists.txt
-
     repl1=$(cat <<- "EOF"
-  '' +
-"# Generate CMake package configuration for BALL build tree
-CONFIGURE_FILE(
-        \"\${PROJECT_SOURCE_DIR}/cmake/BALLConfig.cmake.in\"
-        \"\${PROJECT_BINARY_DIR}/cmake/BALLConfig.cmake\"
-        @ONLY
-)
+## Create BALLExport.cmake
+STRING(COMPARE LESS ''${CMAKE_MINOR_VERSION} "8" CMAKE_DEPRECATED_VERSION)
+IF (''${CMAKE_DEPRECATED_VERSION})
+	MESSAGE(STATUS "Cannot register BALL with CMake! For external code, set the path to BALL during find_package() manually.")
+ELSE()
+	## register BALL with CMake so that it can be found easily
+	EXPORT(PACKAGE BALL)
+ENDIF()
 EOF
 )
-" + ''
-    
 
-    substituteInPlace CMakeLists.txt --replace "''${Python3_LIBRARIES}" "" \
-      --replace "$repl1" ""
+    substituteInPlace CMakeLists.txt --replace "$repl1" ""
   '';
 
   cmakeFlags = (if useCUDA then [ "-DUSE_CUDA=YES" ] else []) ++ [ "-DBALL_LICENSE=GPL" "-DUSE_MPI=YES" ];
