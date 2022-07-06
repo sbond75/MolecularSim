f00262bfa0bb37746aeb8c6063c63ff2395358a9
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-06 13:19:38 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/libsball.nix
+++ LAMMPS_Testing/libsball.nix
@@ -14,8 +14,8 @@ stdenv.mkDerivation rec {
   
   installPhase = ''
     mkdir $out
-    install -Dm755 testsball $out/bin
-    install -Dm755 libsball.a $out/lib
+    install -Dm755 -d testsball $out/bin
+    install -Dm755 -d libsball.a $out/lib
     
     mkdir $out/include
     cp *.h $out/include/
