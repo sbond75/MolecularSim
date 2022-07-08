1658a42cbd85177c2d3474dbbc5ae01ea8d5d806
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-08 15:05:45 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/xdrfile.nix
+++ LAMMPS_Testing/xdrfile.nix
@@ -7,6 +7,9 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ cmake ];
 
+  patchPhase = ''
+    substituteInPlace CMakeLists.txt --replace "file(COPY test_data DESTINATION ''${CMAKE_CURRENT_BINARY_DIR})" "file(COPY test_data DESTINATION ${out})"
+
   src = fetchFromGitHub {
     owner = "chemfiles";
     repo = name;
