f8ea2d7c8b231631aea3fee8de9615e8cf81a1ca
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-08 14:59:30 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/plumed.nix
+++ LAMMPS_Testing/plumed.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, gnumake, gnupatch, python, mpich, blas, lapack, callPackage, graphviz, zlib, fetchPypi, buildPythonPackage, darwin, xxd, pkg-config, gfortran,
+{ lib, stdenv, fetchFromGitHub, gnumake, gnupatch, python, mpich, blas, lapack, callPackage, graphviz, zlib, fetchPypi, buildPythonPackage, darwin, xxd, pkg-config, gfortran, gsl, fftw, python3, bash-completion
 
   intelCompilers ? {}
 }:
@@ -7,15 +7,18 @@
 # checking for libmolfile_plugin.h... no
 # configure: WARNING: cannot enable __PLUMED_HAS_EXTERNAL_MOLFILE_PLUGINS`
 # `}
+let
+  vmd = (callPackage ./vmd.nix {fetchPypi=fetchPypi; buildPythonPackage=buildPythonPackage; intelCompilers=intelCompilers;});
+in
 stdenv.mkDerivation rec {
   name = "plumed2";
   version = "2.7.4";
 
-  buildInputs = [ gnumake python mpich blas lapack (callPackage ./vmd.nix {fetchPypi=fetchPypi; buildPythonPackage=buildPythonPackage; intelCompilers=intelCompilers;}) (callPackage ./doxygen.nix {CoreServices=darwin.apple_sdk.frameworks.CoreServices;}) graphviz zlib xxd pkg-config gfortran ];
+  buildInputs = [ gnumake python mpich blas lapack vmd (callPackage ./doxygen.nix {CoreServices=darwin.apple_sdk.frameworks.CoreServices;}) graphviz zlib xxd pkg-config gfortran gsl fftw python3 bash-completion (callPackage ./xdrfile.nix {}) ];
 
-  CPATH = lib.makeSearchPathOutput "dev" "include" buildInputs; # https://github.com/NixOS/nix/issues/3276
+  CPATH = (lib.makeSearchPathOutput "dev" "include" buildInputs) + ":${vmd.outPath}/plugins/LINUXAMD64/molfile"; # https://github.com/NixOS/nix/issues/3276
   # https://gist.github.com/CMCDragonkai/8b5cc041cea4a7e45a9cb89f849eaaf8 #
-  LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+  LIBRARY_PATH = lib.makeLibraryPath buildInputs + ":${vmd.outPath}/plugins/LINUXAMD64/molfile";
   #LD_LIBRARY_PATH = lib.makeLibraryPath propagatedBuildInputs;
   # #
   
@@ -32,6 +35,12 @@ stdenv.mkDerivation rec {
       patchShebangs "$i"
     done
   '';
+
+  # To prevent {`
+  # configure: WARNING: dependencies tracking disabled - always make clean before make
+  # configure: Now we will check compulsory headers and libraries`
+  # `}
+  preBuild = ''make clean'';
 
   src = fetchFromGitHub {
     owner = "plumed";
---
+++ LAMMPS_Testing/xdrfile.nix
@@ -0,0 +1,16 @@
+{ lib, stdenv, fetchFromGitHub, cmake
+}:
+
+stdenv.mkDerivation rec {
+  name = "plumed2";
+  version = "0d12c950bcc37bda591f7c5ce8256a281b74bd6d";
+
+  buildInputs = [ cmake ];
+
+  src = fetchFromGitHub {
+    owner = "chemfiles";
+    repo = name;
+    rev = "0d12c950bcc37bda591f7c5ce8256a281b74bd6d";
+    sha256 = "07paji77azw9lz468cbg5456qnwdg84jb3gyg0jr099xhq0gpp34";
+  };
+}
