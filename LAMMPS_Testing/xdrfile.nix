a0494aa4d549c1853016eed43c11f405f06dbdc6
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-09 21:27:11 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/xdrfile.nix
+++ LAMMPS_Testing/xdrfile.nix
@@ -8,15 +8,26 @@ stdenv.mkDerivation rec {
   buildInputs = [ cmake ];
 
   patchPhase = ''
-    substituteInPlace CMakeLists.txt --replace 'file(COPY test_data DESTINATION ''${CMAKE_CURRENT_BINARY_DIR})' "execute_process(COMMAND ls -la $src)
-message(STATUS file(COPY $src/test_data DESTINATION $out))
-file(COPY $src/test_data DESTINATION $out)"
+    substituteInPlace CMakeLists.txt --replace 'if (''${CMAKE_SOURCE_DIR} STREQUAL ''${CMAKE_CURRENT_SOURCE_DIR})
+    enable_testing()
+
+    add_executable(xdrfile_test src/xdrfile_c_test.c $<TARGET_OBJECTS:xdrfile>)
+    target_include_directories(xdrfile_test PRIVATE include)
+    if (UNIX)
+        target_link_libraries(xdrfile_test m)
+    endif()
+
+    add_test(xdrfile xdrfile_test)
+    if(NOT EXISTS "''${CMAKE_CURRENT_BINARY_DIR}/test_data")
+        file(COPY test_data DESTINATION ''${CMAKE_CURRENT_BINARY_DIR})
+    endif()
+endif()' ""
   '';
 
   src = fetchFromGitHub {
     owner = "chemfiles";
     repo = name;
-    rev = "0d12c950bcc37bda591f7c5ce8256a281b74bd6d";
+    rev = version;
     sha256 = "0nyacck6r2li9qi8r7c6wagpd29sbdg21b7bdd84yq6mraja7fs4";
   };
 }
