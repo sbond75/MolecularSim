0f18c1d6af0ebd02e2704e96bc8d6cc2e12cd2af
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-09 22:00:19 -0500

    Tweaking which LAMMPS packages are enabled (and getting newer LAMMPS version etc.)

--- LAMMPS_Testing/xdrfile.nix
+++ LAMMPS_Testing/xdrfile.nix
@@ -21,8 +21,8 @@ stdenv.mkDerivation rec {
     if(NOT EXISTS "''${CMAKE_CURRENT_BINARY_DIR}/test_data")
         file(COPY test_data DESTINATION ''${CMAKE_CURRENT_BINARY_DIR})
     endif()
-endif()' 'install(TARGETS xdrfile DESTINATION ''${CMAKE_INSTALL_PREFIX})
-install(DIRECTORY include DESTINATION ''${CMAKE_INSTALL_PREFIX})' # https://stackoverflow.com/questions/50047946/how-do-you-fix-cmake-make-install-no-rule-to-make-target-install , https://cmake.org/cmake/help/latest/command/install.html
+endif()' 'install(TARGETS xdrfile LIBRARY DESTINATION lib
+INCLUDES DESTINATION include)' # https://stackoverflow.com/questions/50047946/how-do-you-fix-cmake-make-install-no-rule-to-make-target-install , https://cmake.org/cmake/help/latest/command/install.html
   '';
 
   src = fetchFromGitHub {
